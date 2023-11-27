f242a1b8ca54ce2cc5782fd4d44f0f3feaf6dc68
charlis96 <ciarlohernan@gmail.com>
2023-11-27 12:34:25 -0300

    para que ande el front en el contenedor

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0', // Escucha en todas las interfaces de red
+        port: 5173, // El puerto que deseas exponer, si es diferente del predeterminado
+        // Agregar esta línea si estás detrás de un proxy y estás experimentando problemas de websocket
+        // hmr: { 
+        //   clientPort: 80
+        // }
+    },
+});
